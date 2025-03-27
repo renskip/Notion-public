27403e65554719d3bd9ec967835b68a2965b0b61
renskip <38847493+renskip@users.noreply.github.com>
2025-03-27 14:18:30 +1100

    Update figma-affirmations.js

--- figma-affirmations.js
+++ figma-affirmations.js
@@ -58,9 +58,9 @@ function getDailyAffirmation() {
   return affirmations[hash];
 }
 
-const widget = document.createElement("div");
 widget.id = "affirmation-widget"; // Assign an ID for styling
 widget.innerText = getDailyAffirmation();
 
 document.body.appendChild(widget);
+
 
